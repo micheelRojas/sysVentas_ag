7d44b46ef776bc13426db9b1c84352f576d2df54
zipusbraid91 <32888455+zipusbraid91@users.noreply.github.com>
2022-06-11 11:36:47 -0500

    Eliminacion doble importacion de CategoryResolver

--- src/graphQLSchema.js
+++ src/graphQLSchema.js
@@ -29,8 +29,6 @@ import invoiceResolvers from "./sysVentas/facturacion/resolvers";
 import authResolvers from "./sysVentas/authentication/resolvers";
 import {authMutations, authTypeDef} from "./sysVentas/authentication/typeDefs";
 
-import categoryResolvers from './sysVentas/categorias/resolvers';
-
 // merge the typeDefs
 const mergedTypeDefs = mergeSchemas(
 	[
